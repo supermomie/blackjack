9b8d2732755aa8e54eece1762b3ea8f6be7f9fa4
supermomie <fakhredinedev3.0@gmail.com>
2020-02-06 15:16:11 +0100

    initialize loop game and adding title

--- gtkblackjack.py
+++ gtkblackjack.py
@@ -1,1 +1,15 @@
 import pygame
+
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("myBlackJack")
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
