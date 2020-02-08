2c3c00d0591575f9056ce1be9682869c05876a16
supermomie <fakhredinedev3.0@gmail.com>
2020-02-08 11:27:35 +0100

    starting change code spaghetti to clean code

--- blackjack.py
+++ blackjack.py
@@ -3,18 +3,18 @@ from random import randint
 from termcolor import colored
 
 
-CARDNUM = 52
+NUM_CARD = 52
+COMMAND = ["oui", "o", "yes", "y", "mise", "passe"]
+MSG = ["win", "lose"]
 
 
 def command():
-    print("tu joue ?")
+    print("tu veux jouer ?")
     incommand = input()
     if incommand == "oui" or incommand == "o" or incommand == "yes" or incommand == "y":
         start_game()
-    if incommand == "":
-        print(colored('QUIT', 'red'))
-    else: 
-        print(colored('QUIT', 'red'))
+    quitGame = print(colored('QUIT', 'red'))
+    quitGame if incommand == "" else quitGame
 
 
 
@@ -32,46 +32,37 @@ def get_card(cardnum):
 
 
 def sum_values(t = 1, a_c = [], a_j = []):
-    card = get_card(CARDNUM)
+    incommand = input()
+    card = get_card(NUM_CARD)
     _elem = [elem for elem in list(card.values())]
     _player = [player for player in list(card.items())]
-    numeroC = _elem[0][0]
-    numeroJ = _elem[1][0]
+    numeroC, numeroJ = _elem[0][0], _elem[1][0]
     if numeroC == 11 or numeroC == 12 or numeroC == 13 or numeroJ == 11 or numeroJ == 12 or numeroJ == 13:
-        numeroC = 10
-        numeroJ = 10
-    allElemC = a_c
-    allElemJ = a_j
+        numeroC, numeroJ = 10, 10
+    allElemC, allElemJ = a_c, a_j
     allElemC.append(numeroC) if sum(allElemC) < 16 else numeroC
-    allElemJ.append(numeroJ)
-    _sumC = sum(allElemC)
-    _sumJ = sum(allElemJ)
+    allElemJ.append(numeroJ) #if incommand == "oui" else numeroJ
+    _sumC, _sumJ = sum(allElemC), sum(allElemJ)
     print(_player[0][0], allElemC,"=", _sumC)
     print(_player[1][0], allElemJ,"=", _sumJ)
     print("\n\n"+colored('CONTINUE ??', 'green')+"\n\n")
     stateGame = False if _sumJ > 21 or _sumC > 21 else True
     stateUser = _player[0][0] if _player[0][0] == True  else _player[1][0]
-    if _sumJ == 21:
-        print(colored("BLACKJACK POUR "+_player[1][0].upper()+" A WIN LA GAME", 'green'))
-        exit()
-    if _sumC == 21:
-        print(colored("BLACKJACK POUR "+_player[0][0].upper()+" A WIN LA GAME", 'green'))
-        exit()
-    if _sumC > 21:
-        print(colored("GAME OVER POUR "+ _player[0][0].upper(), 'red'))
-        exit()
-    if _sumJ > 21:
-        print(colored("GAME OVER POUR "+ _player[1][0].upper(), 'red'))
-        exit()
-    incommand = input()
+    
+    winJMessage = colored("BLACKJACK POUR "+_player[1][0].upper()+" A WIN LA GAME", 'green')
+    winCMessage = colored("BLACKJACK POUR "+_player[0][0].upper()+" A WIN LA GAME", 'green')
+    gameOverJMessage = colored("GAME OVER POUR "+ _player[0][0].upper(), 'red')
+    gameOverCMessage = colored("GAME OVER POUR "+ _player[1][0].upper(), 'red')
+    print(stateGame)
+    #TODO pass the state to the messages
+    print(winCMessage if stateGame == False else "")
+    winC = winCMessage, exit() if _sumC == 21 else winCMessage
+    winJ = winJMessage, exit() if _sumJ == 21 else winJMessage
+    gameOJ = gameOverJMessage, exit() if _sumJ > 21 else gameOverJMessage
+    gameOC = gameOverCMessage, exit() if _sumC > 21 else gameOverCMessage
     if incommand == "o" or incommand == "oui" or incommand == "y" or incommand == "yes":
         t += 1
         sum_values(t, a_c, a_j)
-
-def rules():
-    sum_values(t, a_c, a_j)
-
-
 
 
 if __name__ == "__main__":
