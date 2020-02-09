5cbd88e3f7c6a2846741a11d787d7bbd24e1ce5c
supermomie <fakhredinedev3.0@gmail.com>
2020-02-09 14:42:49 +0100

    continuing to clean code and minimizing numbers of lines

--- blackjack.py
+++ blackjack.py
@@ -14,9 +14,7 @@ def command():
     if incommand == "oui" or incommand == "o" or incommand == "yes" or incommand == "y":
         start_game()
     quitGame = print(colored('QUIT', 'red'))
-    quitGame if incommand == "" else quitGame
-
-
+    quitGame, exit() if incommand == "" else quitGame
 
 def start_game():
     sum_values()
@@ -32,7 +30,6 @@ def get_card(cardnum):
 
 
 def sum_values(t = 1, a_c = [], a_j = []):
-    incommand = input()
     card = get_card(NUM_CARD)
     _elem = [elem for elem in list(card.values())]
     _player = [player for player in list(card.items())]
@@ -40,26 +37,24 @@ def sum_values(t = 1, a_c = [], a_j = []):
     if numeroC == 11 or numeroC == 12 or numeroC == 13 or numeroJ == 11 or numeroJ == 12 or numeroJ == 13:
         numeroC, numeroJ = 10, 10
     allElemC, allElemJ = a_c, a_j
-    allElemC.append(numeroC) if sum(allElemC) < 16 else numeroC
-    allElemJ.append(numeroJ) #if incommand == "oui" else numeroJ
+    allElemC.append(numeroC) #if sum(allElemC) < 16 else numeroC
+    allElemJ.append(numeroJ) #if incommand == "o" else numeroJ
     _sumC, _sumJ = sum(allElemC), sum(allElemJ)
     print(_player[0][0], allElemC,"=", _sumC)
     print(_player[1][0], allElemJ,"=", _sumJ)
     print("\n\n"+colored('CONTINUE ??', 'green')+"\n\n")
-    stateGame = False if _sumJ > 21 or _sumC > 21 else True
-    stateUser = _player[0][0] if _player[0][0] == True  else _player[1][0]
-    
-    winJMessage = colored("BLACKJACK POUR "+_player[1][0].upper()+" A WIN LA GAME", 'green')
-    winCMessage = colored("BLACKJACK POUR "+_player[0][0].upper()+" A WIN LA GAME", 'green')
-    gameOverJMessage = colored("GAME OVER POUR "+ _player[0][0].upper(), 'red')
-    gameOverCMessage = colored("GAME OVER POUR "+ _player[1][0].upper(), 'red')
-    print(stateGame)
-    #TODO pass the state to the messages
-    print(winCMessage if stateGame == False else "")
-    winC = winCMessage, exit() if _sumC == 21 else winCMessage
-    winJ = winJMessage, exit() if _sumJ == 21 else winJMessage
-    gameOJ = gameOverJMessage, exit() if _sumJ > 21 else gameOverJMessage
-    gameOC = gameOverCMessage, exit() if _sumC > 21 else gameOverCMessage
+
+    stateGame = True if _sumC > 21 else False if _sumJ > 21 else "B" if _sumC == 21 else "B" if _sumJ == 21 else False
+    #TODO correct the stateGame variable
+    print("S-G",stateGame)
+    displayMessage = True if stateGame == True else False
+    playerStatement = _player[1][0] if stateGame == True else _player[0][0]
+    winMessage = colored("LE "+ playerStatement.upper() +" A WIN LA GAME", 'green')
+    gameOverMessage = colored("gameover pour "+ playerStatement.upper(), 'red')
+    result = winMessage if stateGame == False else gameOverMessage
+    print(result) if displayMessage else ""
+    exit() if stateGame == True else ""
+    incommand = input()
     if incommand == "o" or incommand == "oui" or incommand == "y" or incommand == "yes":
         t += 1
         sum_values(t, a_c, a_j)
